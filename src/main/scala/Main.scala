e39aa50e8781b0fac92c781ebc276ab1ddba6f92
Li Zhiguang <zhiguang.li@studio.unibo.it>
2023-08-17 11:42:40 +0200

    WordCount.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,17 @@
 
 object Main {
 
-  val exc: String = constant.EXCLUDE_PATH
-  val path: String = constant.DATASET_CSV_PATH
-
   def main(args: Array[String]): Unit = {
 
     // import dataset and print
-    val wordExtracted = CSVManager.importer(path)
+    val wordExtracted = CSVManager.importer(constant.DATASET_CSV_PATH)
     CSVManager.print(wordExtracted,1, 8)
     println(wordExtracted.size)
 
     // wordCount
     wordExtracted.slice(0, 6).foreach(println)
-    wordExtracted.slice(0, 6).foreach(n => println(WordCount.wordCountScala(n._1 + " " + n._2 + " " + n._3 + " " + n._4)))
+    wordExtracted.foreach(n => println(WordCount.wordCountScala(n._1 + " " + n._2 + " " + n._3 + " " + n._4)))
     //            ^rows from 0 to 6   ^current row                          ^ title     ^subtitle     ^ tag        ^description
+
   }
 }
