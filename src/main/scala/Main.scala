d14407687a556364059da77893f6d2954d8b61ac
zhiguangli <zhiguang.li@studio.unibo.it>
2023-10-13 16:07:47 +0200

    add comments, and don't to change it.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,24 @@
 import scala.collection.mutable.ListBuffer
+
 object Main {
 
   def main(args: Array[String]): Unit = {
 
     // import dataset and print
-    val wordExtracted = CSVManager.importCsv(constant.DATASET_CSV_PATH, List(2, 3, 5, 7) )
+    val wordExtracted = CSVManager.importCsv(constant.DATASET_CSV_PATH, List(2, 3, 5, 7))
     println(wordExtracted.size)
+    wordExtracted.slice(0, 6).foreach(println)
 
     // wordCount
-    wordExtracted.slice(0, 6).foreach(println)
     val queryPath = constant.QUERY_PATH
     val keywords = QueryManager.getQuery(queryPath).map(_.toLowerCase)
 
-    // convert from a list of unmutable Maps to a ListBuffer of mutable maps
+    // convert from a list Maps to a ListBuffer of mutable maps
     val datalist = new ListBuffer[collection.mutable.Map[String, String]]()
-    wordExtracted.foreach(map => datalist += map.to(collection.mutable.Map )) //let's fill the ListBuffer
-    TfIdfCalc.idfTfCalc(keywords, datalist.toList) //.toList = converts from ListBuffer to List
+    wordExtracted.foreach(map => datalist += map.to(collection.mutable.Map))
+
+    // document ranking using TF-IDF function.
+    TfIdfCalc.idfTfCalc(keywords, datalist.toList)
 
   }
 }
