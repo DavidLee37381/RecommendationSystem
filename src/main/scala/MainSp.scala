a87f95ca7680921d88f7d2ec5d52aacfb595daec
Wynn-chan <wynndachan@email.com>
2023-11-03 18:56:33 +0100

    mainsp

--- src/main/scala/MainSp.scala
+++ src/main/scala/MainSp.scala
@@ -7,19 +7,17 @@ object MainSp {
       .master("local[*]")
       .getOrCreate()
 
-    try {
       // import dataset and print
       val wordExtracted = CSVManagerSp.importSP(constant.DATASET_CSV_PATH, spark)
       println(wordExtracted)
-
       val queryPath = constant.QUERY_PATH
       val keywords = QueryManager.getQuery(queryPath).map(_.toLowerCase)
 
-      TfIdfCalcSp.tfIdfCalcSP(keywords, wordExtracted, spark, 20, 10)
-    } finally {
+      TfIdfCalcSp.tfIdfCalcSP(keywords, wordExtracted, spark, 6810, 10)
+
       if (spark != null) {
         spark.stop()
       }
-    }
+
   }
 }
